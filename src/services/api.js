57ffd38add6873af3a77fab3511210003743d939
Ankush Bisht <111033560+ankubisht987@users.noreply.github.com>
2026-05-02 12:39:53 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL || "https://task-manager-backend-production-e668.up.railway.app/api"
 });
 
 export default API;
